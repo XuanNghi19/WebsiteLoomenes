ade790d6fe963284f93fbff33f97aad52d6ba212
Tran Quang Ha <haqt2003@gmail.com>
2023-11-22 23:06:29 +0700

    upload cart-page

---
+++ src/app/cart-page/cart-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'Loomenes-cart-page',
+  templateUrl: './cart-page.component.html',
+  styleUrls: ['./cart-page.component.css']
+})
+export class CartPageComponent {
+
+}
