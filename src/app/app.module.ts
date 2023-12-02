55d8d3e5abc7890b31ed10e3863cf7736ece54c5
XuanNghi19 <XuanNghi19t@outlook.com>
2023-12-03 00:42:56 +0700

    change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { HeaderNavigatorComponent } from './widgets/header-navigator/header-navigator.component';
 import { FooterComponent } from './widgets/footer/footer.component';
 import { ServicesPageComponent } from './services-page/services-page.component';
-import { DraftComponent } from './draft/draft.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +15,6 @@ import { DraftComponent } from './draft/draft.component';
     HeaderNavigatorComponent,
     FooterComponent,
     ServicesPageComponent,
-    DraftComponent
   ],
   imports: [
     BrowserModule,
